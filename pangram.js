1b0915483c4473ce7cdf79c2347b96b7b2f80c16
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-11-22 12:35:34 -0800

    Implement naive pangram solution

---
+++ pangram.js
@@ -0,0 +1,48 @@
+function pangrams(s) {
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+
+  if (s.length < 26) {
+    return "not pangram";
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (alphabet[alphabet.indexOf(s[i].toLowerCase())]) {
+      delete alphabet[alphabet.indexOf(s[i].toLowerCase())];
+    }
+  }
+
+  for (let j = 0; j < alphabet.length; j++) {
+    if (alphabet[j] !== undefined) {
+      return "not pangram";
+    }
+  }
+
+  return "pangram";
+}
