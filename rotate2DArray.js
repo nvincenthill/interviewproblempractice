9c3fe399178e24658378ba613548fe3131d2ccd6
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-11-25 14:07:23 -0800

    Implement rotate2DArray

--- rotate2DArray.js
+++ rotate2DArray.js
@@ -1,9 +1,17 @@
-// Rotate a two-demensional array
-let rotate2DArray = (arr) => {
-    let newArr = [];
-    // implement
-    return newArr;
-}
+// Rotate a 2D array clockwise
+let rotate2DArray = arr => {
+  let newArr = [];
+  let rows = arr[0].length;
+  let cols = arr.length;
+  for (let i = 0; i < rows; i++) {
+    let newRow = [];
+    for (let j = 0; j < arr.length; j++) {
+      newRow.unshift(arr[j][i]);
+    }
+    newArr.push(newRow);
+  }
+  return newArr;
+};
 
 let array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-console.log(rotate2DArray(array));
+console.log(rotate2DArray(array)); // => [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
