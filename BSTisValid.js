183e91694aad95a3f8c9200ceb5346e9778da8ad
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-12-02 13:40:17 -0800

    Implement BSTisValid method

---
+++ BSTisValid.js
@@ -0,0 +1,114 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+// I - tree
+// O - boolean (if tree is valid binary search tree)
+// C - O(n) time and O(n) space
+// E - tree === null || tree === undefined
+
+function isBinarySearchTree(treeRoot) {
+  // Start at the root, with an arbitrarily low lower bound
+  // and an arbitrarily high upper bound
+  const nodeAndBoundsStack = [];
+  nodeAndBoundsStack.push({
+    node: treeRoot,
+    lowerBound: Number.NEGATIVE_INFINITY,
+    upperBound: Number.POSITIVE_INFINITY
+  });
+
+  // Depth-first traversal
+  while (nodeAndBoundsStack.length) {
+    const { node, lowerBound, upperBound } = nodeAndBoundsStack.pop();
+
+    // If this node is invalid, we return false right away
+    if (node.value <= lowerBound || node.value >= upperBound) {
+      return false;
+    }
+
+    if (node.left) {
+      // This node must be less than the current node
+      nodeAndBoundsStack.push({
+        node: node.left,
+        lowerBound,
+        upperBound: node.value
+      });
+    }
+
+    if (node.right) {
+      // This node must be greater than the current node
+      nodeAndBoundsStack.push({
+        node: node.right,
+        lowerBound: node.value,
+        upperBound
+      });
+    }
+  }
+
+  // If none of the nodes were invalid, return true
+  // (At this point we have checked all nodes)
+  return true;
+}
+
+// Tests
+
+let desc = "valid full tree";
+let treeRoot = new BinaryTreeNode(50);
+let leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+let rightNode = treeRoot.insertRight(70);
+rightNode.insertLeft(60);
+rightNode.insertRight(80);
+assertEquals(isBinarySearchTree(treeRoot), true, desc);
+
+desc = "both subtrees valid";
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(20);
+leftNode.insertRight(60);
+rightNode = treeRoot.insertRight(80);
+rightNode.insertLeft(70);
+rightNode.insertRight(90);
+assertEquals(isBinarySearchTree(treeRoot), false, desc);
+
+desc = "descending linked list";
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(40);
+leftNode = leftNode.insertLeft(30);
+leftNode = leftNode.insertLeft(20);
+leftNode = leftNode.insertLeft(10);
+assertEquals(isBinarySearchTree(treeRoot), true, desc);
+
+desc = "out of order linked list";
+treeRoot = new BinaryTreeNode(50);
+rightNode = treeRoot.insertRight(70);
+rightNode = rightNode.insertRight(60);
+rightNode = rightNode.insertRight(80);
+assertEquals(isBinarySearchTree(treeRoot), false, desc);
+
+desc = "one node tree";
+treeRoot = new BinaryTreeNode(50);
+assertEquals(isBinarySearchTree(treeRoot), true, desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
