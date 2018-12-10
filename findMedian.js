660789d96ea40fcbab5142e74186925adc93d767
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-12-10 13:23:38 -0800

    Implement findMedian

--- findMedian.js
+++ findMedian.js
@@ -1,9 +1,10 @@
-function arrayMedian(seq) {
-  let sorted = seq.sort((a, b) => a - b);
+function findMedian(seq) {
+  seq.sort((a, b) => a - b);
   let midpoint = Math.floor(seq.length / 2);
   if (seq.length % 2 === 0) {
-    return (sorted[midpoint] + sorted[midpoint - 1]) / 2;
+    let avg = seq[midpoint] + seq[midpoint - 1];
+    return avg / 2;
   } else {
-    return sorted[midpoint];
+    return seq[midpoint];
   }
 }
