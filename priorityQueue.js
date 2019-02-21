64c6dce08b507623e0c6056dedaedc7e74e71bbc
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-21 09:31:24 -0800

    Add hint to priorityQueue

--- priorityQueue.js
+++ priorityQueue.js
@@ -1,12 +1,13 @@
-// Implement a special queue that has three priority values (0, 1, 2), and it dequeues in increasing order (0 --> 1 --> 2).
-// If no values are in the queue, return null. If there are two or more values of the same priority number, dequeue the
-// first value that was inserted into the special queue.
+// Implement a "special" queue that has three priority values (0, 1, 2), and it dequeues in increasing order (0 --> 1 --> 2). If no values are in the queue, return null. If there are two or more values of the same priority number, dequeue the first value that was inserted into the special queue.
+
+// Hint #1
+// When you call enqueue() the queue adds an object with a priority and a value. When dequeue() is called the objects leave the queue in priority order one at a time on each invocation (all high priority items leave first, then all medium priority items, then all low priority items).
 
 const SpecialQueue = function() {
   this.storage = {
-    "0": [],
-    "1": [],
-    "2": []
+    '0': [],
+    '1': [],
+    '2': []
   };
 };
 
@@ -15,7 +16,7 @@ SpecialQueue.prototype.enqueue = function(value, priority) {
   if (priority >= 0 && priority <= 2) {
     this.storage[priority].push(value);
   } else {
-    return "Priority level not allowed";
+    return 'Priority level not allowed';
   }
 };
 
@@ -34,9 +35,9 @@ SpecialQueue.prototype.dequeue = function() {
 
 var queue = new SpecialQueue();
 
-queue.enqueue("blah", 2);
-queue.enqueue("blahblah", 1);
-queue.enqueue("blahblahblah", 0);
+queue.enqueue('blah', 2);
+queue.enqueue('blahblah', 1);
+queue.enqueue('blahblahblah', 0);
 
 console.log(queue.dequeue()); // 'blahblahblah'
 console.log(queue.dequeue()); // 'blahblah'
