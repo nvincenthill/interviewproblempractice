ae887c44d3c265442672d9daac2548737fc15000
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-27 13:38:04 -0800

    Add IOCE

--- 4Sum.js
+++ 4Sum.js
@@ -18,6 +18,11 @@
 // 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
 // 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
 
+// I - Four arrays full of integers
+// O - number (number of distinct combinations of values that sum to zero, one from each array)
+// C - O(n^2) time complexity
+// E - arrays with length zero
+
 const fourSumCount = function(A, B, C, D) {
   const map = {};
 
