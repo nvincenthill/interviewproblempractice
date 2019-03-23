30de84e1995e6298e7109702897153fc9257193e
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-23 10:11:07 -0700

    Add largestSquare

---
+++ largestSquare.js
@@ -0,0 +1,11 @@
+// I - 2D array filled with 1's and 0's
+// O - area of largest square of 1's (number)
+// C - none
+// E - array length zero - return zero
+
+const largestSquare = matrix => {};
+
+// tests
+
+// let arr = [[0, 1, 1, 0], [0, 1, 1, 1], [0, 1, 0, 0], [0, 0, 0, 0]];
+// console.log(largestSquare(arr)); // 4
