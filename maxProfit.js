dac97e77395ab13ac577bc1ab5cdd1eda1b0997b
mypersonalgithubaccount <nvincenthill@gmail.com>
2019-09-02 21:21:39 -0700

    Lint code

--- maxProfit.js
+++ maxProfit.js
@@ -1,13 +1,13 @@
-function getMaxProfit(stockPrices) {
-  if (stockPrices.length < 2) {
-    throw "Error";
+function getMaxProfit(prices) {
+  if (prices.length < 2) {
+    throw 'Error';
   }
 
-  let maxProfit = stockPrices[1] - stockPrices[0];
-  let minPrice = stockPrices[0];
+  let maxProfit = prices[1] - prices[0];
+  let minPrice = prices[0];
 
-  for (let i = 1; i < stockPrices.length; i++) {
-    const currentPrice = stockPrices[i];
+  for (let i = 1; i < prices.length; i++) {
+    const currentPrice = prices[i];
     const potentialProfit = currentPrice - minPrice;
     maxProfit = Math.max(maxProfit, potentialProfit);
     minPrice = Math.min(minPrice, currentPrice);
@@ -18,36 +18,36 @@ function getMaxProfit(stockPrices) {
 
 // Tests
 
-let desc = "price goes up then down";
+let desc = 'price goes up then down';
 let actual = getMaxProfit([1, 5, 3, 2]);
 let expected = 4;
 assertEqual(actual, expected, desc);
 
-desc = "price goes down then up";
+desc = 'price goes down then up';
 actual = getMaxProfit([7, 2, 8, 9]);
 expected = 7;
 assertEqual(actual, expected, desc);
 
-desc = "price goes up all day";
+desc = 'price goes up all day';
 actual = getMaxProfit([1, 6, 7, 9]);
 expected = 8;
 assertEqual(actual, expected, desc);
 
-desc = "price goes down all day";
+desc = 'price goes down all day';
 actual = getMaxProfit([9, 7, 4, 1]);
 expected = -2;
 assertEqual(actual, expected, desc);
 
-desc = "price stays the same all day";
+desc = 'price stays the same all day';
 actual = getMaxProfit([1, 1, 1, 1]);
 expected = 0;
 assertEqual(actual, expected, desc);
 
-desc = "error with empty prices";
+desc = 'error with empty prices';
 const emptyArray = () => getMaxProfit([]);
 assertThrowsError(emptyArray, desc);
 
-desc = "error with one price";
+desc = 'error with one price';
 const onePrice = () => getMaxProfit([1]);
 assertThrowsError(onePrice, desc);
 
